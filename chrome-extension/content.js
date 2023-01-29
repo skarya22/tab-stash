b2208aba8574140f63ba89da5a89c69d88914b8c
Selena Jin <selenajin9@gmail.com>
2023-01-28 19:02:29 -0500

    get content from website

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,21 @@
-console.log("HIIII");
+var port = chrome.runtime.connect({ name: "knockknock" });
+
+window.addEventListener("load", function () {
+  let bodyText = document.body.innerText;
+  port.postMessage({ joke: bodyText });
+  console.log(bodyText);
+});
+
+chrome.extension.onRequest.addListener(function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.method == "getSelection") {
+    console.log(window.getSelection().toString());
+    sendResponse({ data: window.getSelection().toString() });
+  } else {
+    console.log("no selection");
+    sendResponse({}); // snub them.
+  }
+});
