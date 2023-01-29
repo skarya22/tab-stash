2d3edfea3424e312576fc77d68aa21526bf2c610
Saagar Arya <saagar.arya@mail.mcgill.ca>
2023-01-29 02:25:04 -0500

    init signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,13 @@ import "material-symbols";
 import Home from "./pages/Home";
 import SignInPage from "./pages/SignInPage";
 
-const isLoggedIn = true;
+var isLoggedIn = false;
 
 function App() {
+  if (localStorage.getItem("token") != null) {
+    isLoggedIn = true;
+  }
+
   if (isLoggedIn) {
     return <Home />;
   } else {
