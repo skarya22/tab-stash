67d4837462b5c10ae35062eb33d36e1981b78d3e
jada-ke <jada.thompson915@gmail.com>
2023-01-29 02:05:37 -0500

    cohere_ai and nlp complete

---
+++ backend/tabstashapp/app/nlp.py
@@ -0,0 +1,27 @@
+from revChatGPT.ChatGPT import Chatbot
+
+chatbot = Chatbot({"session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..34Q4TFtUwgiTYH9N.8j4J3zZZFu9kLckt-VwDqtcsIcL8Zw-OsAAxf3vRk53ycbJJPX2xvGGMkZWjLzfLmR4pm5mm9aeVLyWpVHmONwQ6FfOCyPd9rG_hS-FHl44OfD4EWTZlZ5m4y-5tz4G0tykfmjwIxQfiynl3UqdujubRcLNYAYl-R1zcsbdEvp5RtMCJQ4JA24yhChjcCHUvo-rh-GUK95QcPcQ1168g3o0JYGJUbWSJ7WnZmrwy4EA3rvMS_ku-moRUvXf1BIkpZyrGtEKT1EM52qL3ztibdSphb5bwP4JJu95oO1OAPVJ62a070SXoct8W94abqVj_lAkoc01ecyERUWqsVbxfkzvB7ddDI7uFkFQoEJ3pREJ2SO32Ou_MjYHAU57PYLVesTvRz8x_ekK9YCiCNhtypjuMzLPEXnwXNy0wYBYOXeLnBZPTLU6rty7lnJcE7WmsNUB8KsxhAeinY3aM4pGRJSCNAOFendEsb-vInoSlNUnY62LLE9cKPzjWQwKvmYtQWZvyRxYMBiWH3RvFr77rR7zIJLiEjugZYFfKaDPC_PhQEfUGSNzHd4O8FkAWdI4BpDmh-Y7gAyqnTfCuo7407xdBKztqlWrpqtPKXC5w-tJT8X46tniaFQRscXOxYTkwquwlw9AMIO5KX5RWqoxYJIWhTy4m0OoPkj1Ex5bQ7ONw3ybhKlr0taNsDBU0O_Z-xD7dK0th49u9cUCt85xn3sh2Hf7O3WRuY-VkCXV27Fp_ZGDr4jfhaHosOCqRlLfOX7XCrfqRYjR1-Hdo3B-SMPh4zoQJMMkg7WEqaNNr_ExrY7UjxXmQ09mt_fstqASTykEcD_yH0cGL_Z1V84n19DvERKI1OHq08W89fKDRzxiThhovcUZLmg94rA0K-3Hqoh2y4_Nkh9hOB2Bd9ZsBWay3aQZadnVOR6QUwNPBQJZ1vM_8EY8E9JZTxcoBJyETP4jn10abEF42BTSxn-W7Xan9PcU8eettvpbY-jd3tj8uLlalT1f8lpqmZmBpVEwqCMSNSflhiDPH6z0PiHLzKtX0j5Pe6g0KK-ybh2LJ4s-Ov89uNJdGfwmm4A_FYaQ84CtPR2X735m2MKLmvm5KSQXPGMr5piBf9xusBBLbzx17KbZu3XTZWrzKCGGg8zDuyqXJRlJSM6yxUNyLq2c1K0md2ltORBAeQJnvhc55R9BmKIsDwxk5F5EDNr5nR8G9dORHETwu0Gsp9SrC-8usL0PuUuB1MsWA4CtBCmr8cnD2JJHYPIPKI7AvN81fKeZs31kXoc5xJCXxvPSSN59Xi08L8aOp-g1QN1BpoNVMtOcUSa__lo4e-G9OKW0tc29W-nu4v17HLFU9bnvXc5w_yIs8NMbc5rc4JtTIrmP2TOrPqRETEvFvQIOj1dPYiyX4B5k282AenreqXpby5kljblj0LI6hQfkO78CKQjTdd6Nq4SeS7UJlweUz_ht1TvHwgy2luAqdGfEPUTS4QVx-R7KLgc7o2_4uUz2D7E29nCXyBPAoc3XriOGPSpEzTq3bVLRqXm1M6u75tKuTsL_opo7c9oyVy1NwhcT9mAsxc-S8zN8zNc0r62xSfgA8vGf8VEwKxWp81q68WbFBP_a6FxXUzuEVLGNw4agnND7Y6B-y6hiMhqE7Y77Gc4f1jkmC8kUw96hgnAFSeeXhPtlUAehhtBTmIfIXfwoQBmzRfVp5JNfuz2Tt4LyuejQbf-oEBHZiPB0mlAkdKBA41nnTXKeCu02BkuZeeE2C0oYlboRQTZC8xDOdKN8ldCnYCGisGlZ7Mykj89X-h7OhSz2HsSrLAYEQGobCEotqew0WtPgGBsivTYbKQpCeLQdr_j6mDn4FoqtLcV6ScorcsXKXKEUg6kQjCAqpQQp83xgddYhhEx3GZXmvrWsVWdVnDcuevzfzHrU5uH3B52JEAuhpqD6hrJd08VRfQHyDlY99ZGGJ0LUAgmwT0wycSe_MfCiPUNSbhLitRhKX3r_Du2woxbKw_tIj54yqDK9tK0v8tKOiLzPWTPtcDPDEcgfnLjOomYGappGURdkZcLtsKlJ55EJYHialOFUOxsKlAxWKypUkYAwHUxCd-MIp3BhPGRrNLUqXvc4UGwniI1I5en9QMRn4E0NbFmJICAPsx2jsFQI8ascFxcylMs_ABCq2ZCuETmyRxjhVC0q9UY_npGQhyDM-q-v8AQ5F8kDrw9y1XN7CAPQS3vfGnsUJ_I6l-nAeWRc10QD-SoKpzjX_oqe1mOdMV19pYKK1GoEZduuEPsfye0THXZS2fsj2aPGezDlEvynlSA6jWRnYjf1c-aF_XKSJjJL7yDL3cexLrQ6q2vxSZfCzX1wjf__gBeQxQFCDX67pmQV1lkiIEZlzB9iO0JigeLD08k_6pUQ7MQA-oHC4tlhtCyWA7YBW02wkA4o_cqODVW-1jWZZGaDD1Hlxxzqm6SdShAUBX-O3yrcwcq421-y75FomuG7v6Z1ASuwAs5jI7klPPd5b4yThM_3L2zdqcNE1zovaBoxQyMnACYDG3rn4clinvTqPNeiUUlJoPm4n877dYmw.EKs6n36luElSwdcczx7IYw"})
+
+
+def summarize(text):
+   
+    prompt = "Summarize this text: " 
+    summary = chatbot.ask( prompt+ text, conversation_id=None, parent_id=None)
+    # print(summary.get("message"))
+
+    return summary.get("message")
+
+def qna(text, question):
+    prompt = "Answer this question based on the text: "
+    answer = chatbot.ask(prompt + question + "\n" + " Text: " + text, conversation_id=None, parent_id=None)
+    # print(answer.get("message"))
+
+    return answer.get("message")
+
+if __name__ == "__main__":
+    test_text = "The platypus (Ornithorhynchus anatinus),[3] sometimes referred to as the duck-billed platypus, is a semiaquatic, egg-laying mammal endemic to eastern Australia, including Tasmania. The platypus is the sole living representative or monotypic taxon of its family (Ornithorhynchidae) and genus (Ornithorhynchus), though a number of related species appear in the fossil record.Together with the four species of echidna, it is one of the five extant species of monotremes, mammals that lay eggs instead of giving birth to live young. Like other monotremes, it senses prey through electrolocation. It is one of the few species of venomous mammals, as the male platypus has a spur on the hind foot that delivers a venom, capable of causing severe pain to humans. The unusual appearance of this egg-laying, duck-billed, beaver-tailed, otter-footed mammal baffled European naturalists when they first encountered it, and the first scientists to examine a preserved platypus body (in 1799) judged it a fake, made of several animals sewn together."
+    test_question = "How does the platypus sense preys?"
+    # test_text = "The platypus is cute."
+    # test_question = "What animal is cute?"
+    #summarize(test_text)
+    print(qna(test_text, test_question))
